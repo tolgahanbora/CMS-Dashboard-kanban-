1cc8237f12b04063e048f9f76c978a2b61febc1e
tolgahanbora <tolgahanbora@outlook.com>
2023-04-05 18:27:42 +0300

    Added license key

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { registerLicense } from '@syncfusion/ej2-base';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
+
+registerLicense(process.env.REACT_APP_SYNCFUSION_LICENSE);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
+  // eslint-disable-next-line no-multiple-empty-lines
 
-    <ContextProvider>
+  <ContextProvider>
     <App />
-    </ContextProvider>
+  </ContextProvider>,
 
-  
 );
 
